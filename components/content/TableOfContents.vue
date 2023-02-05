7d8a74e675528131a423b268847cc1b01adac4b7
uto-usui <me@uto-usui.com>
2023-02-05 20:30:39 +0900

    ✨ add TableOfContents

---
+++ components/content/TableOfContents.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <ul v-if="toc && toc.links">
+      <li v-for="link in toc.links" :key="link.text">
+        <a :href="`#${link.id}`">
+          {{ link.text }}
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const { toc } = useContent()
+</script>
+
+<style scoped></style>
